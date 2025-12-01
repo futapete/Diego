3d85f2ee11768dcbb324cd53e9236624c5ec68b4
futapete <diegleo09@gmail.com>
2025-12-01 17:59:29 +0800

    Update page.tsx

--- app/page.tsx
+++ app/page.tsx
@@ -50,7 +50,7 @@ export default function Home() {
           <div className="flex-1 space-y-8">
             <div className="space-y-4">
               <h1 className="text-5xl md:text-7xl font-bold tracking-tighter text-balance">
-                Developing clean and accessible <br/>
+                Building modern <br/>
                 <span className="text-zinc-400 dark:text-zinc-600">web experiences.</span>
               </h1>
               <p className="text-xl text-zinc-600 dark:text-zinc-400 max-w-lg leading-relaxed text-balance">
